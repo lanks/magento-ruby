710704734e736bdb7aed6e6dcc860a10b3ce72a7
Liam Houlahan <liamhoulahan@gmail.com>
2023-04-06 16:16:17 +1200

    Attempt to add product links to the product.

--- lib/magento/product.rb
+++ lib/magento/product.rb
@@ -6,6 +6,10 @@ module Magento
       attr(m) || super(m, *params, &block)
     end
 
+    def product_links
+      extension_attributes&.product_links
+    end
+
     def stock
       extension_attributes&.stock_item
     end
